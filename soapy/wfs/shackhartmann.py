1df46d5709b987c3d2b76b8b314a6ce047097d5f
Gilles Orban de Xivry <gorban@ulg.ac.be>
2018-11-13 13:15:46 +0100

    revert to LGS FFTW backward for iFFT

--- soapy/wfs/shackhartmann.py
+++ soapy/wfs/shackhartmann.py
@@ -199,9 +199,15 @@ class ShackHartmann(base.WFS):
                     (self.n_subaps, self.subapFFTPadding, self.subapFFTPadding), dtype=CDTYPE)
             self.iFFT = pyfftw.FFTW(
                 self.ifft_input_data, self.ifft_output_data, axes=(-2, -1),
+<<<<<<< HEAD
                 threads=self.threads, flags=(self.config.fftwFlag, "FFTW_DESTROY_INPUT")) #,
                 # direction="FFTW_BACKWARD"
                 # )
+=======
+                threads=self.threads, flags=(self.config.fftwFlag, "FFTW_DESTROY_INPUT"),
+                direction="FFTW_BACKWARD"
+                )
+>>>>>>> revert to LGS FFTW backward for iFFT
 
             self.lgs_ifft_input_data = pyfftw.empty_aligned(
                 (self.subapFFTPadding, self.subapFFTPadding), dtype=CDTYPE)
@@ -209,9 +215,15 @@ class ShackHartmann(base.WFS):
                 (self.subapFFTPadding, self.subapFFTPadding), dtype=CDTYPE)
             self.lgs_iFFT = pyfftw.FFTW(
                 self.lgs_ifft_input_data, self.lgs_ifft_output_data, axes=(0, 1),
+<<<<<<< HEAD
                 threads=self.threads, flags=(self.config.fftwFlag, "FFTW_DESTROY_INPUT")) #,
                 # direction="FFTW_BACKWARD"
                 # )
+=======
+                threads=self.threads, flags=(self.config.fftwFlag, "FFTW_DESTROY_INPUT"),
+                direction="FFTW_BACKWARD"
+                )
+>>>>>>> revert to LGS FFTW backward for iFFT
 
     def initLGS(self):
         super(ShackHartmann, self).initLGS()
